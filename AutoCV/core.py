f117e4dde29aa3d54900c7e908d7dc9513655746
kerem <keremozrtk@gmail.com>
2024-02-13 14:02:51 +0300

    Init CV conditions

--- AutoCV/core.py
+++ AutoCV/core.py
@@ -7,7 +7,11 @@ from sklearn.model_selection import (
     cross_validate as sklearn_cv,
     StratifiedKFold,
     KFold,
-    GroupKFold
+    GroupKFold,
+    LeaveOneOut,
+    LeavePOut,
+    ShuffleSplit,
+    StratifiedShuffleSplit
 )
 from sklearn.utils.multiclass import type_of_target
 from sklearn.preprocessing import LabelEncoder
@@ -60,21 +64,35 @@ class AutoCV:
         class_counts = np.bincount(y)
         minority_class_ratio = np.min(class_counts) / np.sum(class_counts)
         return minority_class_ratio < 0.3
+    
+    def _determine_n_splits(self):
+        pass
 
-    def cross_validate(self, X, y):
+    def cross_validate(self, X, y, force=False):
         """
         Perform automatic cross-validation.
 
         Parameters:
         X: array-like, shape (n_samples, n_features), the input data
         y: array-like, shape (n_samples,), the target variable
+        force: bool, optional (default=False), If True, forces cross-validation on large datasets.
 
         Returns:
         dict: A dictionary containing the mean and standard deviation of the cross-validation scores.
         """
+        size = X.shape[0]
+
+        if size > 100000 and not force:
+            print("Dataset is large. Skipping cross-validation. Set `force=True` to override.")
+            return None
+        
         problem_type = self._determine_problem_type(y)
 
-        if problem_type == 'classification':
+        if size <= 200:
+            cv_strategy = LeaveOneOut()
+        elif size <= 1000:
+            cv_strategy = LeavePOut(p=np.round(size/100))
+        elif problem_type == 'classification':
             # Encode labels if they are not numeric
             if y.dtype == 'O' or isinstance(y[0], str):
                 y = LabelEncoder().fit_transform(y)
@@ -82,28 +100,24 @@ class AutoCV:
             # Check if data is imbalanced
             if self._is_imbalanced(y):
                 print("Data is imbalanced. Using Stratified K-Fold.")
-                cv_strategy = StratifiedKFold(n_splits=self.cv)
-            else:
-                cv_strategy = KFold(n_splits=self.cv)
+                if size < 20000:
+                    cv_strategy = StratifiedKFold(n_splits=self.cv)
+                else:
+                    cv_strategy = StratifiedShuffleSplit(n_splits=self.cv)
         else:
-            cv_strategy = KFold(n_splits=self.cv)
+            if size < 20000:
+                cv_strategy = KFold(n_splits=self.cv)
+            else:
+                cv_strategy = ShuffleSplit(n_splits=self.cv)
 
         # Use Group K-Fold if specified
-        if self.use_group_kfold:
-            if self.group_column is not None:
-                cv_strategy = GroupKFold(n_splits=self.cv)
-                groups = self.group_column
-            else:
-                raise ValueError("Group column must be provided for Group K-Fold cross-validation.")
-        else:
-            groups = None
+        if self.group_column is not None:
+            cv_strategy = GroupKFold(n_splits=self.cv)
+            groups = self.group_column
 
-        print("Starting cross-validation...")  # Debug statement
         scores = sklearn_cv(self.model, X, y, cv=cv_strategy, scoring=self.scoring, groups=groups)
-        print(f"Scores: {scores}")  # Debug statement
         results = {
             'mean_score': np.mean(scores),
             'std_score': np.std(scores)
         }
-        print(f"Results: {results}")  # Debug statement
         return results
