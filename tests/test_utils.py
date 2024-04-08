b65b969811d631b47c1d91b5e8e690cbd75ba612
kerem <keremozrtk@gmail.com>
2024-04-08 18:20:33 +0300

    Test determine_problem_type

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,19 @@
 import pytest
-import autocv.utils
+import numpy as np
+
+from autocv.utils import (
+    determine_problem_type,
+    is_sklearn_model,
+    is_tf_model,
+    is_torch_model,
+    check_estimator_type
+)
+
+@pytest.mark.parametrize("target, expected, expected_detailed", [
+    (np.array([0, 1, 0, 1]), 'classification', 'binary_classification'),
+    (np.array([0, 1, 2, 1]), 'classification', 'multiclass_classification'),
+    (np.array([1.5, 2.3, 3.8, 4.1]), 'regression', 'regression')
+])
+def test_determine_problem_type(target, expected, expected_detailed):
+    assert determine_problem_type(target) == expected
+    assert determine_problem_type(target, detailed=True) == expected_detailed
