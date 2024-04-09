cf2eed5ff7e0cd2b206ed8b7aa6feec2127a35d9
kerem <keremozrtk@gmail.com>
2024-04-09 21:49:01 +0300

    Add more utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,3 +17,17 @@ from autocv.utils import (
 def test_determine_problem_type(target, expected, expected_detailed):
     assert determine_problem_type(target) == expected
     assert determine_problem_type(target, detailed=True) == expected_detailed
+
+def test_determine_problem_type_unsupported():
+    # Unsupported type test
+    target = np.array([[0, 1], [1, 0]])  # Multilabel indicator
+    with pytest.raises(ValueError):
+        determine_problem_type(target)
+
+def test_is_sklearn_model():
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression()
+    assert is_sklearn_model(model) == True
+
+    model = np.array([0, 1, 1])
+    assert is_sklearn_model(model) == False
