aa9239b6906f9df3451a0408240a82bd4b1aa15e
kerem <keremozrtk@gmail.com>
2024-04-11 19:22:34 +0300

    Add more utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -31,3 +31,52 @@ def test_is_sklearn_model():
 
     model = np.array([0, 1, 1])
     assert is_sklearn_model(model) == False
+
+def test_is_torch_model():
+    try:
+        from torch.nn import Module
+        class TestModel(Module):
+            pass
+        model = TestModel()
+        assert is_torch_model(model) == True
+    except:
+        assert True
+
+def test_is_tf_model():
+    try:
+        from tensorflow.keras.models import Sequential
+        model = Sequential()
+        assert is_tf_model(model) == True
+    except:
+        assert True
+
+def test_check_estimator_type_sklearn():
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression()
+    assert check_estimator_type(model) == 'ml'
+
+def test_check_estimator_type_torch():
+    try:
+        from torch.nn import Module
+        class TestModel(Module):
+            pass
+        model = TestModel()
+        assert check_estimator_type(model) == 'nn'
+    except:
+        assert True
+
+def test_check_estimator_type_tf():
+    try:
+        from tensorflow.keras.models import Sequential
+        model = Sequential()
+        assert check_estimator_type(model) == 'nn'
+    except:
+        assert True
+
+def test_check_estimator_type_unsupported():
+    # Test unsupported estimator type
+    class UnsupportedModel:
+        pass
+    model = UnsupportedModel()
+    with pytest.raises(TypeError):
+        check_estimator_type(model)
